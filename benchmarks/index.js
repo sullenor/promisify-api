447358b5fc26791974e6e557969590bde946012a
Alexey Litvinov <sullenor@yandex-team.ru>
2016-04-12 19:52:08 +0300

    pretty output

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,8 +1,12 @@
 var readdir = require('fs').readdirSync;
 var resolve = require('path').resolve;
-var suite = new require('benchmark').Suite();
 
 var cases = resolve(__dirname, 'cases');
+var suite = new require('benchmark').Suite({
+  onCycle: function (e) {
+    console.log(String(e.target));
+  }
+});
 
 readdir(cases).forEach(function (name) {
   var testCase = require(resolve(cases, name));
@@ -10,15 +14,7 @@ readdir(cases).forEach(function (name) {
   suite.add(name, {
     defer: testCase.length > 0,
     fn: testCase,
-  })
+  });
 });
 
-suite
-.on('complete', function () {
-  this.forEach(function (benchmark) {
-    // https://benchmarkjs.com/docs#prototype_stats
-    console.log(benchmark.name);
-    console.log(' → The sample arithmetic mean (secs): ' + benchmark.stats.mean);
-  });
-})
-.run({async: true});
+suite.run();
